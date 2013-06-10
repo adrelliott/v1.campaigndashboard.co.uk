8c959767264d90f0084af6bb386366af13b9d7f0
Al <user@Als-MacBook-Pro.local>
2013-06-10 19:00:16 +0200

    updating database settings

--- application/config/staging/database.php
+++ application/config/staging/database.php
@@ -64,10 +64,10 @@ $db['staging']['swap_pre'] = '';
 $db['staging']['autoinit'] = TRUE;
 $db['staging']['stricton'] = FALSE;
 
-$db['production']['hostname'] = '10.168.1.57';
+$db['production']['hostname'] = '10.168.1.56';
 $db['production']['username'] = 'campaign2_master';
 $db['production']['password'] = 'r7g63S*UZ^n?';
-$db['production']['database'] = 'campaign2_master';
+$db['production']['database'] = 'campaign2_production';
 $db['production']['dbdriver'] = 'mysql';
 $db['production']['dbprefix'] = '';
 $db['production']['pconnect'] = TRUE;
