67cce6e243e6c110f7715a8fb07367d32ffd819e
Al <user@Als-MacBook-Pro.local>
2013-06-10 16:00:29 +0200

    debug

---
+++ application/controllers/test.php
@@ -0,0 +1,28 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/**
+ * Controller - test
+ * @author Al Elliott
+ * 
+ * XXXXXXXXXXDescription_goes_hereXXXXXXXXXX
+ * 
+ */
+class Test extends CI_Controller {
+
+    public $model_name = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    function index() {
+        echo "hello";
+    }
+
+}
+
+/* End of file test.php */
+/* Location: ./application/controllers/XXXXXXXXXXXXXXXXXXXX/test.php */
